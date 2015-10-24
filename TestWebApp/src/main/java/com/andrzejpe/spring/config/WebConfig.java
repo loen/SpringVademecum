e838140bd689d1d0039d54c1541b8b641886a2f0
jedrek <andrzej.pozlutko@gmail.com>
2015-10-24 14:20:17 +0200

    JSTL utils playground

--- TestWebApp/src/main/java/com/andrzejpe/spring/config/WebConfig.java
+++ TestWebApp/src/main/java/com/andrzejpe/spring/config/WebConfig.java
@@ -1,8 +1,10 @@
 package com.andrzejpe.spring.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -26,6 +28,15 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         viewResolver.setViewClass(JstlView.class);
         viewResolver.setExposeContextBeansAsAttributes(true);
         return viewResolver;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource =
+                new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setFallbackToSystemLocale(true);
+        return messageSource;
     }
 
     @Override
--- TestWebApp/src/main/webapp/WEB-INF/views/home.jsp
+++ TestWebApp/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,13 +1,17 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="s"%>
 <%@page session="false" %>
 <html>
     <head>
-        <title>TestWebApp</title>
+        <title><s:message code="home.title" /></title>
         <link rel="stylesheet" type="text/css" href="<c:url value="/resources/style.css" />">
     </head>
-    <body>
     <h2>Welcome in TestWebApp</h2>
-    <a href="<c:url value="/spittles"/>">Spittles</a> |
-    <a href="<c:url value="/spittles/register"/>">Register</a>
+    <a href="<s:url value="/spittles"/>">Spittles</a> |
+    <a href="<s:url value="/spittles/register"/>">Register</a>
+    </br>
+    <s:escapeBody htmlEscape="true">
+        <h1>escapeExample</h1>
+    </s:escapeBody>
     </body>
 </html>
