1b15befea9f7513c4f88d081e60ec4272b9c648c
Andrzej Pozlutko <andrzej.pozlutko@gmail.com>
2016-03-30 22:26:03 +0200

    Web socket example initial commit

--- WebSocketExample/src/main/java/com/andrzejpe/spring/config/WebConfig.java
+++ WebSocketExample/src/main/java/com/andrzejpe/spring/config/WebConfig.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.andrzejpe.spring.web")
